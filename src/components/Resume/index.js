5988e2be1053e92a7802ec39c1da658f9dc3d8ff
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2022-12-16 12:21:11 -0300

    Added cards in Resume

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import './style.scss'
 import { ResumeItem } from '../ResumeItem'
+import {FaRegArrowAltCircleUp, FaRegArrowAltCircleDown, FaDollarSign} from 'react-icons/fa'
+
 
 const Resume = () => {
   return (
     <section className='Resume'>
-      <ResumeItem />
-      <ResumeItem />
-      <ResumeItem />
+      <ResumeItem title="Entrance" Icon={FaRegArrowAltCircleUp} value="1000" />
+      <ResumeItem title="Exits" Icon={FaRegArrowAltCircleDown} value="1000" />
+      <ResumeItem title="Total" Icon={FaDollarSign} value="1000" />
     </section>
   )
 }
--- src/components/ResumeItem/index.js
+++ src/components/ResumeItem/index.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import './style.scss'
 
-export const ResumeItem = () => {
+export const ResumeItem = ({title, Icon, value}) => {
   return (
     <div className='ResumeItem'>
       <div className='ResumeItem__header'>
-        <p className='ResumeItem__header__title'>Teste</p>
+        <p className='ResumeItem__header__title'>{title}</p>
+        <Icon />
       </div>
 
-      <div className='ResumeItem__total'>8000</div>
+      <div className='ResumeItem__total'>{value}</div>
     </div>
   )
 }
